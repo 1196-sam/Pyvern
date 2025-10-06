855f76b7e17153a94af498a689516dc401430cc3
1196-sam <sambritcliffe15@gmail.com>
2025-10-07 00:58:25 +0100

    new file commited :)

---
+++ many socket server.py
@@ -0,0 +1,95 @@
+try:
+    import os,random,datetime,traceback,socket,threading
+    from datetime import datetime
+    #server
+    self = "server"
+
+
+    class chat():
+        def __init_(self):
+            self.name = ""
+            self.messages = []
+            self.users = []
+
+    receive_data(csc_):#receive message sent to server and send it to all users
+        while True:
+        data = csc.recv(1024)
+        for s in sockets:
+            s.send(data)
+        if data:
+            messages.append(data.decode())
+    
+    send_chat(csc_,messages): #when user connects, send them the chat so far
+        y = str(messages)
+        y = y.encode('utf-8')
+        csc.sendall(y)
+
+    for s in sockets: #run as background thread for each user
+        def socket_manager():
+            receive_data()
+            send_data()
+            ping_connection()
+
+    
+    sockets = []
+    count = 0
+    socket_port=1500
+
+    chats = []
+    
+    for files in os.listdir("."):
+        if "###" in files:
+            new = chat()
+            new.name = files.replace("###","")
+            new.messages = files.unpickle
+            chats.append(new)
+            
+            
+    while True:
+        sockets.append(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
+        socket_ip = '127.0.0.1'
+        sockets[count] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sockets[count].bind((socket_ip,socket_port))
+        sockets[count].listen(16)
+        
+        csc, address=sockets[0].accept()
+        #send_chat(csc_,messages)
+        count += 1
+        socket_port += 1
+        print(len(sockets))
+    
+except Exception as error:
+    traceback.print_exc()
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+    
