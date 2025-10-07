b869a54f158d245f5529ecc42af183e838a35fa4
oli <oliverpedley2@gmail.com>
2025-10-07 13:12:03 +0100

    Fixed Setup.py

--- setup.py
+++ setup.py
@@ -19,59 +19,78 @@ print("""
 # Welcome to PyChat Setup! #
 ############################
 """)
-serverrantime = 0
+import os
+
 def ServerSetup():
-    global serverrantime
-    if serverrantime == 0:
-        print("Downloading Server Files...")
+    if not os.path.exists('server.py'):
+        print("server.py not found downloading server files.")
         os.system("curl -O https://raw.githubusercontent.com/1196-sam/py-lan-chat/refs/heads/main/server.py")
     else:
-        import server
-        files = os.listdir('.')
-        if 'backup.txt' in files:
-            print('message backup found')
-            if 'config.json' in files:
-                print('Server config file found.')
-                server.run()
-            else:
-                open('config.json','w')
-                print('config file not found...\nOne has been created for you in the current directory.')
-                serverrantime += 1
-                ServerSetup()
-        else:
-            open('backup.txt','w')
-            print('No backup file found...\n One has been created for you in the current directory.')
-            serverrantime += 1
-            ServerSetup()
+        print("server.py found")
 
-
-
-clientrantime = 0
-def ClientSetup():
-    global clientrantime
-    if clientrantime == 0:
-        print('Downloading Client Files...')
-        os.system("curl -O https://raw.githubusercontent.com/1196-sam/py-lan-chat/refs/heads/main/chat.py")
-        clientrantime += 1
-        ClientSetup()
+    if not os.path.exists('backup.txt'):
+        print("No backup file found creating backup.txt...")
+        with open('backup.txt', 'w') as f:
+            f.write("")  
     else:
-        import chat
-        files = os.listdir('.')
-        if 'secret.json' in files:
-            print('Token found')
-            if 'userconfig.json' in files:
-                print('User generated config found.')
+        print("backup.txt found")
+
+    if not os.path.exists('config.json'):
+        print("config.json not found creating config.json...")
+        with open('config.json', 'w') as f:
+            f.write("{}")  
+    else:
+        print("config.json found")
+
+    if os.path.exists('server.py') and os.path.exists('backup.txt') and os.path.exists('config.json'):
+        print("All files found")
+        answer = input('Would you like to start the Server now?(Answer "Y" or "N")\n>')
+        match answer.lower():
+            case "y":
+                import server
+                server.run()
+            case "n":
+                print('Ok, Server not started. Exiting Setup...')
+            case other:
+                print('Answer not given...\nDefaulting to No...')
+                print('Ok, Server not started. Exiting Setup...')
+
+
+def ClientSetup():
+    if not os.path.exists('chat.py'):
+        print('Chat files not found...\n Downloading Chat files...')
+        os.system("curl -O https://raw.githubusercontent.com/1196-sam/py-lan-chat/refs/heads/main/chat.py")
+    else:
+        print('chat.py found')
+    if not os.path.exists('secret.json'):
+        print('No Token file found.\nCreating Token file.')
+        with open('secret.json','w') as f:
+            f.write("{}")
+    else:
+        print('Token found.')
+        
+    if not os.path.exists('userconfig.json'):
+        print('No Userconfig file found.\nCreating UserConfig file.')
+        with open('userconfig.json','w') as f:
+            f.write("{}")
+    else:
+        print('UserConfig found.')
+
+    if os.path.exists('chat.py') and os.path.exists('secret.json') and os.path.exists('userconfig.json'):
+        print("All files found starting chat...")
+        answer = input('Would you like to start the Chat now?(Answer "Y" or "N")\n>')
+        match answer.lower():
+            case "y":
+                import chat
                 chat.run()
-            else:
-                print('User config not found.\n Generating...')
-                open('userconfig.json',"w")
-                ClientSetup()
-        else:
-            print('Token not found...\n Generating file for token when conected to server.')
-            open('secret.json','w')
-            ClientSetup()
+            case "n":
+                print('Ok, chat not started. Exiting Setup...')
+            case other:
+                print('Answer not given...\nDefaulting to No...')
+                print('Ok, chat not started. Exiting Setup...')
 
 
+        
 
 mode = input('Please type "Y" for server, "N" for client. \n The default is SERVER\n>')
 
@@ -85,4 +104,3 @@ match mode.lower():
 
 
 
-print('Server created')
